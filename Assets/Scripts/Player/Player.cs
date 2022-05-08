47cb2daf437a96d84ba9504a237b8a9d86453cdc
Javier Candeira <javi.candeira@gmail.com>
2022-05-08 14:17:28 +0200

    dontdestroyonload fix

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,16 +7,15 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Animator))]
     public class Player : MonoBehaviour, IDamagable, IKillable
     {
-        public int maxHealth;
+        
         public int maxMana;
-        public Slider Slider;
         private int CurrentHealth { get; set; }
         public double CurrentMana { get;  private set; }
         private void Start()
         {
-        CurrentHealth = maxHealth;
+        
         CurrentMana = maxMana;
-        SetStats(maxHealth);
+        //SetStats(maxHealth);
         }
 
         private void Update()
@@ -29,25 +28,21 @@ using UnityEngine.UI;
 
         private void SetStats(int health)
         {
-            Slider.maxValue = health;
-            Slider.value = health;
+           // Slider.maxValue = health;
+           // Slider.value = health;
         }
         public void SetHealth(int health)
         {
-            Slider.value = health;
+           // Slider.value = health;
         }
 
-        private void OnDisable()
-        {
-            PlayerPrefs.SetInt("health",CurrentHealth);
-        }
-
+        
         public void Damage(int damagePoints)
         {
-            CurrentHealth -= damagePoints;
-            Slider.value = CurrentHealth;
+            PlayerManager.Instance.CurrentHealth -= damagePoints;
+            PlayerManager.Instance.Slider.value = PlayerManager.Instance.CurrentHealth;
             
-            if(CurrentHealth <= 0)
+            if(PlayerManager.Instance.CurrentHealth <= 0)
             {
                 Perish();
             }
--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -2,15 +2,18 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour
 {
     #region Singleton
 
     public static PlayerManager Instance;
-
+    public int CurrentHealth = 100;
+    public Slider Slider;
     private void Awake()
     {
+        DontDestroyOnLoad(this.gameObject);
         Instance = this;
     }
 
